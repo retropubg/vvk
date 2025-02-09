65b22a7f3664c6e4971afc86c0d54d5187441d8d
Rigo Jiménez <70866911+r1gox@users.noreply.github.com>
2025-02-09 12:20:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
-$bot_token = getenv('TELEGRAM_BOT_TOKEN'); // Obtener token de variables de entorno
+$token = getenv('TELEGRAM_BOT_TOKEN'); // Obtener token de variables de entorno
+
+$website = "https://api.telegram.org/bot".$token;
+$upda = json_decode(file_get_contents('php://input'), true);
+$data = file_get_contents("php://input");
+$json = json_decode($data, true);
+$update = $json["message"];
+//---------PERSONAL---------//
+$id = $update["from"]["id"];
+$Name = $update["from"]["first_name"];
+$last = $update["from"]["last_name"];
+$message_id = $update["message_id"];
+$message = $update["text"];
+//----------GRUPOS----------//
+$chat_id = $update["chat"]["id"];
+$id_new = $update["new_chat_member"]["id"];
+$grupo = $update["chat"]["title"];
+$nuevo = $update["new_chat_member"]["first_name"]. ' '.$update["new_chat_member"]["last>
+//----------------------END VARIABLES----------------------//
+
+$user = $update["from"]["username"];
+//------------seguridad-------------//
+// ID de tu usuario (para permitir mensajes personales solo para ti)
+$myid = "1292171163"; // Reemplaza con tu ID de usuario
+
 
 // Configurar webhook automáticamente al visitar /?setup
 /*
@@ -9,20 +33,22 @@ if(isset($_GET['setup'])) {
     die("✅ Webhook configurado!");
 }
 */
-// Solo procesar comandos si es una solicitud POST
-    $update = json_decode(file_get_contents('php://input'), true);
-    $message = $update['message'];
-    $text = $message['text'];
-    $chat_id = $message['chat']['id'];
 
     // Único comando: /start
     if($text === "/start") {
         $respuesta = "👋 ¡Hola! Soy un bot simple.\n\n"
             . "Mi único propósito es saludarte cuando escribes /start 😊";
-        
-        // Enviar respuesta
-        file_get_contents("https://api.telegram.org/bot$bot_token/sendMessage?" . http_build_query([
-            'chat_id' => $chat_id,
-            'text' => $respuesta
-        ]));
+        sendMessage($chat_id, $respuesta, $message_id)
+
     }
+
+
+//-------FUNCION DE ENVIAR---------//
+function sendMessage($chatID, $respuesta, $message_id) {
+$url = $GLOBALS["website"]."/sendMessage?disable_web_page_preview=true&chat_id=".$chatID."&reply_to_message_id=".$message_id."&parse_mode=HTML&text=".urlencode($respuesta);
+//$url = $GLOBALS["website"]."/sendMessage?disable_web_page_preview=true&chat_id=".$chatID."&parse_mode=HTML&text=".urlencode($respuesta);
+$cap_message_id = file_get_contents($url);
+//------------EXTRAE EL ID DEL MENSAGE----------//
+$id_cap = capture($cap_message_id, '"message_id":', ',');
+file_put_contents("ID", $id_cap);
+}
